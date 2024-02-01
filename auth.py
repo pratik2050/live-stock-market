3c339df2d8ea2ed2fc14c74e37961db6fb9cd24a
Pratik Das <pratik14901@gmail.com>
2024-02-02 00:20:06 +0530

    will be getting data

---
+++ auth.py
@@ -0,0 +1,63 @@
+import requests as rq
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import urllib.parse as urlparse
+from pyotp import TOTP
+
+
+#### Login Credentials ####
+
+api_key = 'cddff55d-5354-47d0-86e1-29a4f89c24ab'
+secret_key = 'zks0ehc9jr'
+r_url = 'https://127.0.0.1:5000/'
+totp_key = '978745'
+mobile_no = '9366978745'
+pin = '978745'
+auth_url = f'https://api-v2.upstox.com/login/authorization/dialog?response_type=code&client_id={api_key}&redirect_uri={r_url}'
+
+#https://api-v2.upstox.com/login/authorization/dialog?response_type=code&client_id=cddff55d-5354-47d0-86e1-29a4f89c24ab&redirect_uri=https://127.0.0.1:5000/
+
+sleep(1)
+
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+# options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+driver.get(auth_url)
+wait = WebDriverWait(driver,3)
+wait.until(EC.presence_of_element_located((By.XPATH, '//input[@type="text"]'))).send_keys(mobile_no)
+wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="getOtp"]'))).click()
+totp = TOTP(totp_key).now()
+sleep(2)
+wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="otpNum"]'))).send_keys(totp)
+wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="continueBtn"]'))).click()
+sleep(2)
+wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="pinCode"]'))).send_keys(pin)
+wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="pinContinueBtn"]'))).click()
+sleep(2)
+token_url = driver.current_url
+parsed = urlparse.urlparse(token_url)
+driver.close()
+code = urlparse.parse_qs(parsed.query)['code'][0]
+url = 'https://api-v2.upstox.com/login/authorization/token'
+headers = {
+    'accept': 'application/json',
+    'Api-Version': '2.0',
+    'Content-Type': 'application/x-www-form-urlencoded'}
+
+data = {
+    'code': code,
+    'client_id': api_key,
+    'client_secret': secret_key,
+    'redirect_uri': r_url,
+    'grant_type': 'authorization_code'}
+
+response = rq.post(url, headers=headers, data=data)
+jsr = response.json()
+
+with open('accessToken.txt','w') as file:
+    file.write(jsr['access_token'])
+print(f"Access Token : {jsr['access_token']}")
---
+++ web_socket_data.py
@@ -0,0 +1,97 @@
+# Import necessary modules
+import asyncio
+import json
+import ssl
+import upstox_client
+import websockets
+from google.protobuf.json_format import MessageToDict
+from threading import Thread
+import MarketDataFeed_pb2 as pb
+from time import sleep
+
+filename =f"accessToken.txt"
+with open(filename,"r") as file:
+    access_token = file.read()
+
+def get_market_data_feed_authorize(api_version, configuration):
+    """Get authorization for market data feed."""
+    api_instance = upstox_client.WebsocketApi(
+        upstox_client.ApiClient(configuration))
+    api_response = api_instance.get_market_data_feed_authorize(api_version)
+    return api_response
+
+
+def decode_protobuf(buffer):
+    """Decode protobuf message."""
+    feed_response = pb.FeedResponse()
+    feed_response.ParseFromString(buffer)
+    return feed_response
+
+
+async def fetch_market_data():
+    global data_dict
+    """Fetch market data using WebSocket and print it."""
+
+    # Create default SSL context
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+
+    # Configure OAuth2 access token for authorization
+    configuration = upstox_client.Configuration()
+
+    api_version = '2.0'
+    configuration.access_token = access_token
+
+    # Get market data feed authorization
+    response = get_market_data_feed_authorize(
+        api_version, configuration)
+
+    # Connect to the WebSocket with SSL context
+    async with websockets.connect(response.data.authorized_redirect_uri, ssl=ssl_context) as websocket:
+        print('Connection established')
+
+        await asyncio.sleep(1)  # Wait for 1 second
+
+        # Data to be sent over the WebSocket
+        data = {
+            "guid": "someguid",
+            "method": "sub",
+            "data": {
+                "mode": "full",
+                "instrumentKeys": ["NSE_INDEX|Nifty Bank", "NSE_INDEX|Nifty 50"]
+            }
+        }
+
+        # Convert data to binary and send over WebSocket
+        binary_data = json.dumps(data).encode('utf-8')
+        await websocket.send(binary_data)
+
+        # Continuously receive and decode data from WebSocket
+        while True:
+            message = await websocket.recv()
+            decoded_data = decode_protobuf(message)
+
+            # Convert the decoded data to a dictionary
+            data_dict = MessageToDict(decoded_data)
+
+            # Print the dictionary representation
+            # print(json.dumps(data_dict))
+
+
+# Execute the function to fetch market data
+# asyncio.run(fetch_market_data())
+def run_websocket():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(fetch_market_data())
+
+# Start the WebSocket connection in a separate thread
+websocket_thread = Thread(target=run_websocket)
+websocket_thread.start()
+sleep(5)
+while True:
+    sleep(1)
+    ltp = data_dict['feeds']['NSE_INDEX|Nifty Bank']['ff']['indexFF']['ltpc']['ltp']
+    ltp_50 = data_dict['feeds']['NSE_INDEX|Nifty 50']['ff']['indexFF']['ltpc']['ltp']
+    print(f"Last Price {ltp} : {ltp_50}")
