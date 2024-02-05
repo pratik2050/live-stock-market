80b8a6a36f333051ba5aaaa14c9ad75940b3edf3
Pratik Das <pratik14901@gmail.com>
2024-02-05 19:47:01 +0530

    bank nifty

--- web_socket_data.py
+++ web_socket_data.py
@@ -9,15 +9,22 @@ import websockets
 from google.protobuf.json_format import MessageToDict
 from threading import Thread
 import MarketDataFeed_pb2 as pb
-from time import sleep
-import datetime
+import time
+import requests
 
 
 ### Creating XLSX Records ###
 
-headers = ['Instrument', 'LTP', 'Volume', 'High', 'Low']
+headers = ['Instrument', 'LTP', 'High', 'Low']
 workbook = xw.Book('TickData.xlsx')
 worksheet = workbook.sheets[0]
+
+ohlc_time = time.time()
+ohlc_data = ''
+high_price = ''
+low_price = ''
+open_price = ''
+close_price = ''
 
 filename =f"accessToken.txt"
 with open(filename,"r") as file:
@@ -69,56 +76,7 @@ async def fetch_market_data():
             "method": "sub",
             "data": {
                 "mode": "full",
-                "instrumentKeys": [ "NSE_EQ|INE742F01042",
-    "NSE_EQ|INE029A01011",
-    "NSE_EQ|INE752E01010",
-    "NSE_EQ|INE158A01026",
-    # "NSE_EQ|INE361B01024",
-    # "NSE_EQ|INE002A01018",
-    # "NSE_EQ|INE009A01021",
-    # "NSE_EQ|INE213A01029",
-    # "NSE_EQ|INE214T01019",
-    # "NSE_EQ|INE090A01021",
-    # "NSE_EQ|INE669C01036",
-    # "NSE_EQ|INE467B01029",
-    # "NSE_EQ|INE081A01020",
-    # "NSE_EQ|INE522F01014",
-    # "NSE_EQ|INE733E01010",
-    # "NSE_EQ|INE918I01026",
-    # "NSE_EQ|INE192A01025",
-    # "NSE_EQ|INE123W01016",
-    # "NSE_EQ|INE423A01024",
-    # "NSE_EQ|INE075A01022",
-    # "NSE_EQ|INE481G01011",
-    # "NSE_EQ|INE019A01038",
-    # "NSE_EQ|INE155A01022",
-    # "NSE_EQ|INE917I01010",
-    # "NSE_EQ|INE047A01021",
-    # "NSE_EQ|INE437A01024",
-    # "NSE_EQ|INE239A01024",
-    # "NSE_EQ|INE296A01024",
-    # "NSE_EQ|INE038A01020",
-    # "NSE_EQ|INE101A01026",
-    # "NSE_EQ|INE059A01026",
-    # "NSE_EQ|INE089A01023",
-    # "NSE_EQ|INE860A01027",
-    # "NSE_EQ|INE628A01036",
-    # "NSE_EQ|INE021A01026",
-    # "NSE_EQ|INE044A01036",
-    # "NSE_EQ|INE154A01025",
-    # "NSE_EQ|INE040A01034",
-    # "NSE_EQ|INE216A01030",
-    # "NSE_EQ|INE095A01012",
-    # "NSE_EQ|INE237A01028",
-    # "NSE_EQ|INE280A01028",
-    # "NSE_EQ|INE397D01024",
-    # "NSE_EQ|INE018A01030",
-    # "NSE_EQ|INE062A01020",
-    # "NSE_EQ|INE030A01027",
-    # "NSE_EQ|INE238A01034",
-    "NSE_EQ|INE795G01014",
-    "NSE_EQ|INE585B01010",
-    "NSE_EQ|INE066A01021"]
+                "instrumentKeys": [ "NSE_INDEX|Nifty Bank" ]
             }
         }
 
@@ -150,31 +108,35 @@ def run_websocket():
 websocket_thread = Thread(target=run_websocket)
 websocket_thread.start()
 
-sleep(5)
+time.sleep(5)
 
 while True:
-    sleep(1)
+    time.sleep(1)
 
     data = (MessageToDict(decoded_data))
 
     for instrument, values in data.get('feeds', {}).items():
         # Extract values for each instrument
-        ltp = values.get('ff', {}).get('marketFF', {}).get('ltpc', {}).get('ltp', '')
-                
-        ohlc_data = values.get('ff', {}).get('marketFF', {}).get('marketOHLC', {}).get('ohlc', [])
-        volume = ohlc_data[0].get('volume', '') if ohlc_data else ''
-                
-        high_price = ohlc_data[0].get('high', '') if ohlc_data else ''
-        low_price = ohlc_data[0].get('low', '') if ohlc_data else ''
+        ltp = values.get('ff', {}).get('indexFF', {}).get('ltpc', {}).get('ltp', '')
+        
+        if time.time() - ohlc_time >= 15:
+            ohlc_data = values.get('ff', {}).get('indexFF', {}).get('marketOHLC', {}).get('ohlc', [])
+            open_price = ohlc_data[0].get('open', '') if ohlc_data else ''
+            high_price = ohlc_data[0].get('high', '') if ohlc_data else ''
+            low_price = ohlc_data[0].get('low', '') if ohlc_data else ''
+            close_price = ohlc_data[0].get('close', '') if ohlc_data else ''
+
+            ohlc_time = time.time()
                 
 
         # Append data to the DataFrame
         data_dict = data_dict._append({
             'Instrument': instrument,
             'LTP': ltp,
-            'Volume': volume,
+            'Open': open_price,
             'High': high_price,
-            'Low': low_price
+            'Low': low_price,
+            'Close': close_price
         }, ignore_index=True)
 
         # Update the Excel file with the DataFrame
